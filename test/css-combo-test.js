7a2a2e024c10b64cc207e3bbbbd775dfc7dea392
daxingplay <daxingplay@gmail.com>
2012-05-21 15:19:39 +0800

    0.1.1

---
+++ test/css-combo-test.js
@@ -0,0 +1,13 @@
+﻿var EasyCombo = require('../lib/base').EasyCombo;
+
+EasyCombo.build({
+    base:'D:\\project\\tradeface\\assets\\4.0',
+    debug: true,
+    outputBase:'D:\\project\\tradeface\\assets\\testbuild',
+    output:'tc/cart/cart.combine.css',
+    includes:[
+        'tc/cart/cart.css',
+        'tc/cart/order.css',
+        'tc/cart/item.css'
+    ]
+});
